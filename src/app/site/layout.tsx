889980d860eaf8641d477d2842a887c13c91c71a
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-22 14:07:04 +0530

    Update site\layout.tsx
    The Navigation component was added before passing the children as props within the main tag in the layout file specifically for the site directory. This ensures that the Navigation component is rendered before any child components within the main layout, providing consistent navigation across all pages in the site directory.

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,4 +1,4 @@
-import Navigation from '@/components/ui/site/navigation'
+import Navigation from '@/components/site/navigation'
 import React from 'react'
 
 const layout = ({children}: {children: React.ReactNode}) => {
