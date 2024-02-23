dc88db89b73f5b3d1acf510f44512aaa6f27920a
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-23 14:05:15 +0530

    Update removed ClerkProvider
     Removed the ClerkProvider from the root layout.tsx file because Clerk is only needed on the main directory and the site directory. Instead, wrapped the layout files of both directories with ClerkProvider to ensure Clerk functionality is available where needed.

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from "@clerk/themes"
+import React from 'react'
+
+const Layout = ({children}: {children: React.ReactNode}) => {
+  return (
+  <ClerkProvider appearance={{ baseTheme: dark}}>
+    <div>{children}</div>
+    </ClerkProvider>
+  )
+}
+
+export default Layout
