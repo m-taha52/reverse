260c12a50c0789d6c9c1bdd6d759ad76ceed9e9f
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-26 18:35:07 +0530

    Update queries.ts
    Created a verifyAndAcceptInvitation query. Here's an overview of its purpose and functionality:
    
    Purpose: The query aims to onboard someone who has been invited to the agency.
    
    Functionality:
    
    First, it checks if the user is logged in. If not, they are redirected to the sign-in page.
    Once the user is logged in, their email is used to check the database for any pending invitation.
    If a pending invitation is found, the user's email, agencyId, avatar, name, role, createdAt, and updatedAt are forwarded to the createTeamUser query.
    This query streamlines the process of accepting invitations and onboarding users into the agency

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -2,6 +2,9 @@
 
 import { currentUser } from "@clerk/nextjs"
 import { db } from "./db"
+import { redirect } from "next/navigation"
+import { User } from "@prisma/client"
+
 
 export const getAuthUserDetails = async() => 
 {
@@ -29,4 +32,29 @@ export const getAuthUserDetails = async() =>
           })
 
           return userData
+export const verifyAndAcceptInvitation = async ( ) => 
+{
+    const user = await currentUser();
+    if(!user)
+        return redirect("/sign-in")
+    const invitationExists = await db.invitation.findUnique({
+        where: {
+            email: user.emailAddresses[0].emailAddress,
+            status: "PENDING"
+        }
+    })
+
+    if(invitationExists){
+       const userDetails = await createTeamUser(invitationExists.agencyId, {
+        email: invitationExists.email,
+        agencyId: invitationExists.agencyId,
+        avatarUrl: user.imageUrl,
+        id: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        role: invitationExists.role,
+        createdAt: new Date(),
+        updatedAt: new Date()
+       })
+    }
+
 }
